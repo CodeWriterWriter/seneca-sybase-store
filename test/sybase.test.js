6e5373f8e44992d5e6258e5bd5e88b9519135881
marcopiraccini <marco.piraccini@gmail.com>
2014-12-07 16:01:13 +0100

    Added skeleton of seneca-store-test tests

--- test/sybase.test.js
+++ test/sybase.test.js
@@ -1,15 +1,11 @@
-/*jslint node: true */
-/*global describe:true, it:true */
-/* Copyright (c) 2012 Mircea Alexandru */
 /*
- * These tests assume a MySQL database/structure is already created.
- * execute script/schema.sql to create
+ * Copyright (c) 2014 Marco Piraccini
+ * These tests assume a Sybase SQL Anywhere database/structure is already created.
  */
 
 "use strict";
 
 var seneca = require('seneca');
-var async = require('async');
 var assert = require('assert');
 var shared = require('seneca-store-test');
 var fs = require('fs');
@@ -17,10 +13,30 @@ var fs = require('fs');
 var si = seneca();
 
 var dbConfig;
-if(fs.existsSync(__dirname + '/../test/dbconfig.mine.js')) {
+if (fs.existsSync(__dirname + '/../test/dbconfig.mine.js')) {
   dbConfig = require('./dbconfig.mine');
 } else {
   dbConfig = require('./dbconfig.example');
 }
 
 console.log(dbConfig);
+
+si.__testcount = 0;
+var testcount = 0;
+
+describe('sybase', function () {
+  it('basic', function (done) {
+    testcount++;
+    shared.basictest(si, done);
+  });
+
+//  TODO: Verify if we need extra-tests
+//  it('extra', function (done) {
+//    testcount++;
+//    extra.test(si, done);
+//  });
+
+  it('close', function (done) {
+    shared.closetest(si, testcount, done);
+  });
+});
